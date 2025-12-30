31d6748f7bd9a918a082a46fec407dd0aa4ea519
eduardo <11eduardoalmeida@gmail.com>
2025-12-30 15:56:55 -0300

    Comentários bird7

--- Aula 01/src1/bird7/Bird.lua
+++ Aula 01/src1/bird7/Bird.lua
@@ -1,32 +1,42 @@
+--[[
+    a classe bird controla o personagem.
+    no bird7, adicionamos a funcao collides para detectar colisoes
+    com os canos usando logica aabb (axis-aligned bounding box).
+]]
+
 Bird = Class{}
 
 local GRAVITY = 20
 
 function Bird:init()
-    -- load bird image from disk and assign its width and height
     self.image = love.graphics.newImage('bird.png')
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
 
-    -- position bird in the middle of the screen
     self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
 
-    -- Y velocity; gravity
     self.dy = 0
 end
 
 --[[
-    AABB collision that expects a pipe, which will have an X and Y and reference
-    global pipe width and height values.
+    funcao de colisao aabb que espera receber um cano como parametro.
+    retorna true se houver colisao, false caso contrario.
 ]]
 function Bird:collides(pipe)
-    -- the 2's are left and top offsets
-    -- the 4's are right and bottom offsets
-    -- both offsets are used to shrink the bounding box to give the player
-    -- a little bit of leeway with the collision
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
-        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+    -- definimos offsets (margens) para a nossa caixa de colisao.
+    -- o '+2' e '-4' servem para encolher a caixa de colisao do passaro.
+    -- isso da uma "folga" (leeway) ao jogador, para nao morrer se passar
+    -- raspando nos pixels transparentes ou na borda do sprite.
+    local left = self.x + 2
+    local right = (self.x + 2) + (self.width - 4)
+    local top = self.y + 2
+    local bottom = (self.y + 2) + (self.height - 4)
+
+    -- logica padrao de colisao de retangulos:
+    -- verifica se as bordas do passaro e do cano se sobrepoem
+    if right >= pipe.x and left <= pipe.x + PIPE_WIDTH then
+        if bottom >= pipe.y and top <= pipe.y + PIPE_HEIGHT then
             return true
         end
     end
@@ -35,15 +45,12 @@ function Bird:collides(pipe)
 end
 
 function Bird:update(dt)
-    -- apply gravity to velocity
     self.dy = self.dy + GRAVITY * dt
 
-    -- add a sudden burst of negative gravity if we hit space
     if love.keyboard.wasPressed('space') then
         self.dy = -5
     end
 
-    -- apply current velocity to Y position
     self.y = self.y + self.dy
 end
 
